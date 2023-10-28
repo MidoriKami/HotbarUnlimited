0d5f6c547e01e1fd5fbd7323b2f657223458050f
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-27 19:18:01 -0700

    Use PreDraw

--- HotbarUnlimited/Controllers/HotbarUnlimitedSystem.cs
+++ HotbarUnlimited/Controllers/HotbarUnlimitedSystem.cs
@@ -54,7 +54,7 @@ public unsafe class HotbarUnlimitedSystem : IDisposable {
 
         Service.Framework.Update += OnFrameworkUpdate;
         
-        Service.AddonLifecycle.RegisterListener(AddonEvent.PostUpdate, new[] { "Tooltip", "ActionDetail" }, TooltipOnDraw);
+        Service.AddonLifecycle.RegisterListener(AddonEvent.PreDraw, new[] { "Tooltip", "ActionDetail" }, TooltipOnDraw);
     }
     
     private void TooltipOnDraw(AddonEvent type, AddonArgs args) {
